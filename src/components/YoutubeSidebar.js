39134bbb0f05ee0e88880a01090df3f1ce8fdef0
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2019-08-11 14:17:14 -0400

    Fixed HTML Entities double escaping issue

--- src/components/YoutubeSidebar.js
+++ src/components/YoutubeSidebar.js
@@ -12,7 +12,7 @@ function YoutubeSidebar(props) {
             {props.videos.map(video => <li className='youtube-sidebar-video' key={video.id.videoId} onClick={() => {props.selectHandler(video.id.videoId)}}>
             <img className='youtube-sidebar-video-thumbnail' src={video.snippet.thumbnails.default.url} alt={video.snippet.title}></img>
             <div className='youtube-sidebar-video-info'>
-                <h4 className='youtube-sidebar-video-title'>{video.snippet.title}</h4>
+                <h4 className='youtube-sidebar-video-title' dangerouslySetInnerHTML={{__html: video.snippet.title}}></h4>
                 <p className='youtube-sidebar-video-channel'>{video.snippet.channelTitle}</p>
             </div>
             </li>)}
