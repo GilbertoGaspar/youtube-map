42680722eae0fe0ba08bbad3dfaa5c76db73ece7
Gilberto Gaspar <gilbertogaspar1996@gmail.com>
2021-09-10 14:10:25 -0400

    refactor: use google map's dynamic loading package and fix browser navigator geolocation issues

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,46 +1,99 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+import { connect } from 'react-redux';
 
-import { handleAddLocation } from '../actions/location'
+import { handleAddLocation } from '../actions/location';
+
+const API_KEY = '[YOUR_API_KEY]';
 
 class Map extends Component {
-    componentDidMount() {
-        let currentLocation = {lat: 40.711976, lng: -74.006171 } //New York Fallback
-        const initMap = () => {
-            const google = window.google;
-            var map = new google.maps.Map(
-                document.getElementById('map'), {zoom: 10, center: currentLocation});
-            
-            var marker = new google.maps.Marker({
-                position: currentLocation, 
-                map: map,
-                draggable: true,
-                title: "Drag me!"});
-                marker.addListener('mouseup', ({latLng}) => {
-                    this.props.dispatch(handleAddLocation({
-                        lat: latLng.lat(),
-                        lng: latLng.lng()
-                    }))
+  componentDidMount() {
+    let currentLocation = { lat: 40.711976, lng: -74.006171 }; //New York City Fallback
+
+    const loader = new Loader({
+      apiKey: API_KEY,
+      version: 'weekly',
+    });
+    // let marker = null;
+    loader.load().then((google) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          ({ coords }) => {
+            // If getCurrentPosition was successful, make new map, and marker with current location.
+            currentLocation = { lat: coords.latitude, lng: coords.longitude };
+            const map = new google.maps.Map(document.getElementById('map'), {
+              center: currentLocation,
+              zoom: 10,
+            });
+
+            const marker = new google.maps.Marker({
+              position: currentLocation,
+              map: map,
+              draggable: true,
+              title: 'Drag me!',
+            });
+            marker.addListener('mouseup', ({ latLng }) => {
+              this.props.dispatch(
+                handleAddLocation({
+                  lat: latLng.lat(),
+                  lng: latLng.lng(),
                 })
-            }
+              );
+            });
 
-            //Moves map location to current location if accepted, else returns fallback location.
-        navigator.geolocation.getCurrentPosition(({ coords }) => {
-                currentLocation = {lat: coords.latitude, lng: coords.longitude}
-                this.props.dispatch(handleAddLocation(currentLocation))
-                initMap()
-            },
-            () => {
-                this.props.dispatch(handleAddLocation(currentLocation))
-                initMap()
+            this.props.dispatch(handleAddLocation(currentLocation));
+          },
+          () => {
+            // If geolocation as unsuccessful make new map with fallback location.
+            const map = new google.maps.Map(document.getElementById('map'), {
+              center: currentLocation,
+              zoom: 10,
+            });
+            const marker = new google.maps.Marker({
+              position: currentLocation,
+              map: map,
+              draggable: true,
+              title: 'Drag me!',
+            });
+            marker.addListener('mouseup', ({ latLng }) => {
+              this.props.dispatch(
+                handleAddLocation({
+                  lat: latLng.lat(),
+                  lng: latLng.lng(),
+                })
+              );
+            });
+            this.props.dispatch(handleAddLocation(currentLocation));
+          }
+        );
+      } else {
+        // If navigator browser api doesn't exist, create new map with fallback location.
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: currentLocation,
+          zoom: 10,
+        });
+        const marker = new google.maps.Marker({
+          position: currentLocation,
+          map: map,
+          draggable: true,
+          title: 'Drag me!',
+        });
+        marker.addListener('mouseup', ({ latLng }) => {
+          this.props.dispatch(
+            handleAddLocation({
+              lat: latLng.lat(),
+              lng: latLng.lng(),
             })
-        }
+          );
+        });
+        this.props.dispatch(handleAddLocation(currentLocation));
+      }
+    });
+  }
 
-    render() {
-        return (
-            <div id='map'>Map</div>
-        )
-    }
+  render() {
+    return <div id='map'>Map</div>;
+  }
 }
 
-export default connect()(Map)
+export default connect()(Map);
--- src/components/YoutubeSidebar.js
+++ src/components/YoutubeSidebar.js
@@ -1,25 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 function YoutubeSidebar(props) {
-    if(props.videos.length === 0) {
-        return (
-            <h3>No videos!</h3>
-        )
-    }
-    else {
-        return (
-        <ul className='youtube-sidebar'>
-            {props.videos.map(video => <li className='youtube-sidebar-video' key={video.id.videoId} onClick={() => {props.selectHandler(video.id.videoId)}}>
-            <img className='youtube-sidebar-video-thumbnail' src={video.snippet.thumbnails.default.url} alt={video.snippet.title}></img>
+  if (props.videos.length === 0) {
+    return <h3>No videos in this area!</h3>;
+  } else {
+    return (
+      <ul className='youtube-sidebar'>
+        {props.videos.map((video) => (
+          <li
+            className='youtube-sidebar-video'
+            key={video.id.videoId}
+            onClick={() => {
+              props.selectHandler(video.id.videoId);
+            }}
+          >
+            <img
+              className='youtube-sidebar-video-thumbnail'
+              src={video.snippet.thumbnails.default.url}
+              alt={video.snippet.title}
+            ></img>
             <div className='youtube-sidebar-video-info'>
-                <h4 className='youtube-sidebar-video-title' dangerouslySetInnerHTML={{__html: video.snippet.title}}></h4>
-                <p className='youtube-sidebar-video-channel'>{video.snippet.channelTitle}</p>
+              <h4
+                className='youtube-sidebar-video-title'
+                dangerouslySetInnerHTML={{ __html: video.snippet.title }}
+              ></h4>
+              <p className='youtube-sidebar-video-channel'>
+                {video.snippet.channelTitle}
+              </p>
             </div>
-            </li>)}
-        </ul>
-        )
-    }
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-
-export default YoutubeSidebar
+export default YoutubeSidebar;
